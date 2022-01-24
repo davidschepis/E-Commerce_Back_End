781ac2b6b4e48c0e0306026ade5e133b6f549d8f
David Schepis <davidschepis@gmail.com>
2022-01-23 20:30:57 -0700

    fixed some bugs

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,8 +107,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const data = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(data);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
